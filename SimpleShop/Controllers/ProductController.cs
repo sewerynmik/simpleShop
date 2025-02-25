cdfb13a359571bc5c1330a865316e724c151326f
Seweryn Mik <133761279+sewerynmik@users.noreply.github.com>
2025-02-25 22:18:27 +0100

    starting

---
+++ SimpleShop/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleShop.Models;
+
+namespace SimpleShop.Controllers;
+
+public class ProductController : Controller
+{
+    private static List<Product> products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Laptop", Price = 3000 },
+        new Product {Id = 2, Name = "Telefon", Price = 4000}
+    };
+    
+    // GET
+    public IActionResult Index()
+    {
+        return View(products);
+    }
+}
---
+++ SimpleShop/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleShop.Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=DataBase.db");
+}

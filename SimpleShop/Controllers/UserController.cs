baca6f533aa0447beae9a82d468cd044d2b1b248
Seweryn Mik <133761279+sewerynmik@users.noreply.github.com>
2025-03-02 01:15:16 +0100

    profile done + edit todo

--- SimpleShop/Controllers/UserController.cs
+++ SimpleShop/Controllers/UserController.cs
@@ -28,4 +28,35 @@ public class UserController(AppDbContext context) : Controller
         
         return View(user);
     }
+
+    [Authorize]
+    public IActionResult Edit()
+    {
+        var userId = User.FindFirst("UserId")?.Value;
+
+        if (userId == null) return RedirectToAction("Index", "Home");
+
+        var user = _context.Users.FirstOrDefault(u => u.Id.ToString() == userId);
+
+        if (user == null) return NotFound();
+
+        return View(user);
+    }
+
+    [Authorize]
+    [HttpPut("{id}")]
+    public IActionResult Edit(int id, [FromBody] Users updatedUser)
+    {
+        var user = _context.Users.Find(id);
+
+        if (user == null) return NotFound();
+
+        user.Name = updatedUser.Name;
+        user.Surname = updatedUser.Surname;
+        user.Login = updatedUser.Login;
+        user.Password = updatedUser.Password;
+
+        _context.SaveChanges();
+        return RedirectToAction("Profile");
+    }
 }

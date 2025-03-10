fc8d1dba57fc095d66aa8651333c14ad368787b1
Seweryn Mik <133761279+sewerynmik@users.noreply.github.com>
2025-03-11 00:01:09 +0100

    kupowanie i gotowe

---
+++ SimpleShop/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleShop.Extensions;
+using SimpleShop.Models;
+
+namespace SimpleShop.Controllers;
+
+public class CartController(AppDbContext context) : Controller
+{
+    public IActionResult Index()
+    {
+        var cart = HttpContext.Session.GetObject<List<CartItem>>("Cart") ?? new List<CartItem>();
+        return View(cart);
+    }
+
+    public IActionResult AddToCart(int productId)
+    {
+        var product = context.Products.FirstOrDefault(p => p.Id == productId);
+
+        if (product == null) return NotFound();
+
+        var cart = HttpContext.Session.GetObject<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+        var cartItem = cart.FirstOrDefault(c => c.ProductId == productId);
+
+        if (cartItem != null) cartItem.Quantity++;
+        else
+        {
+            cart.Add(new CartItem
+            {
+                ProductId = product.Id,
+                ProductName = product.Name,
+                Quantity = 1,
+                Price = product.Price
+            });
+        }
+        
+        HttpContext.Session.SetObject("Cart", cart);
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult DeleteFromCart(int productId)
+    {
+        var cart = HttpContext.Session.GetObject<List<CartItem>>("Cart");
+
+        var cartItem = cart.FirstOrDefault(c => c.ProductId == productId);
+
+        if (cartItem == null) return NotFound();
+        
+        cart.Remove(cartItem);
+        
+        HttpContext.Session.SetObject("Cart", cart);
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult ClearCart()
+    {
+        HttpContext.Session.Remove("Cart");
+        return RedirectToAction("Index");
+    }
+    
+}
---
+++ SimpleShop/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleShop.Models;
+
+public class CartItem
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
